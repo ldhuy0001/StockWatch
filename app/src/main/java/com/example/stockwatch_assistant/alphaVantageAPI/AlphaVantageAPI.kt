6c313dceb3068559e3184aeab6e7fd9c872ee520
ldhuy0001 <huyle@utexas.edu>
2022-11-10 15:29:12 -0600

    Update AlphaVantageAPI.kt

--- app/src/main/java/com/example/stockwatch_assistant/alphaVantageAPI/AlphaVantageAPI.kt
+++ app/src/main/java/com/example/stockwatch_assistant/alphaVantageAPI/AlphaVantageAPI.kt
@@ -22,14 +22,14 @@ interface AlphaVantageAPI {
 
     @GET("/query?function=LISTING_STATUS")
     suspend fun getAllActiveListingStocks (
-        @Query("apikey") apikey: String = findAPIKeyAt(count++%15)
+        @Query("apikey") apikey: String = findAPIKeyAt(count++%numberOfAPIKEY)
     ) : ResponseBody
 
 //https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=demo
     @GET("/query?function=OVERVIEW")
     suspend fun getStockDetailsFromAPI(
         @Query("symbol") symbol : String,
-        @Query("apikey") apikey: String = findAPIKeyAt(count++%15)
+        @Query("apikey") apikey: String = findAPIKeyAt(count++%numberOfAPIKEY)
     ) : StockDetails
 
 //https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY
@@ -40,13 +40,13 @@ interface AlphaVantageAPI {
         @Query("symbol") symbol: String,
         @Query("datatype") datatype : String = "csv",
         @Query("interval") interval : String = "5min",
-        @Query("apikey") apikey: String = findAPIKeyAt(count++%15)
+        @Query("apikey") apikey: String = findAPIKeyAt(count++%numberOfAPIKEY)
     ) : ResponseBody
 
 //https://www.alphavantage.co/query?function=NEWS_SENTIMENT&apikey=MY7UOXNLMVAJOBL6
     @GET("query?function=NEWS_SENTIMENT")
     suspend fun getGeneralNews (
-        @Query("apikey") apikey: String = findAPIKeyAt(count++%15)
+        @Query("apikey") apikey: String = findAPIKeyAt(count++%numberOfAPIKEY)
     ) : ListingFeed
 
 //https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&limit=10&apikey=MY7UOXNLMVAJOBL6
@@ -54,14 +54,14 @@ interface AlphaVantageAPI {
     suspend fun getNewsForStock (
         @Query("tickers") tickers : String,
         @Query("limit") limit : String = "10",
-        @Query("apikey") apikey: String = findAPIKeyAt(count++%15)
+        @Query("apikey") apikey: String = findAPIKeyAt(count++%numberOfAPIKEY)
     ) : ListingFeed
 
 //https://www.alphavantage.co/query?function=NEWS_SENTIMENT&tickers=AAPL&limit=10&apikey=MY7UOXNLMVAJOBL6
     @GET("query?function=NEWS_SENTIMENT")
     suspend fun getNewsWithCategory (
         @Query("topics") topics : String,
-        @Query("apikey") apikey: String = findAPIKeyAt(count++%15)
+        @Query("apikey") apikey: String = findAPIKeyAt(count++%numberOfAPIKEY)
 //            API_KEY_FOR_CATEGORY_NEWS
     ) : ListingFeed
 
@@ -89,7 +89,8 @@ interface AlphaVantageAPI {
 //        const val API_KEY_FOR_ALL_STOCKS = "9UF22PMWEV9BPYJ9"
 //        const val API_KEY_FOR_CATEGORY_NEWS = "Z7B9H271PNLIOWHP"
 
-        var count = 0
+        var count = (0..34).random()
+        var numberOfAPIKEY = 35
 
         const val BASE_URL = "https://alphavantage.co"
 
@@ -119,6 +120,27 @@ interface AlphaVantageAPI {
 //                14-> return "YO8GWZOX68E9XL6V"
 
                 //new
+//                0 -> return "AO48IFCXLA3BX1O9"
+//                1 -> return "WA7BKM5HXI56RLF6"
+//                2 -> return "THYSGY8VEFDN4N57"
+//                3 -> return "7II03ZCFLUAXQ3I9"
+//                4 -> return "ZNN7KWS78SORDNHP"
+//                5 -> return "46SD6DKCB9GS2DRX"
+////                66SHOES29PMPXSH8
+//                6 -> return "J3O650830XSJ5OPD"
+//                7 -> return "5FG1ZTL7HT3NSBN9"
+//                8 -> return "PCPMGEIC3XN6PRBG"
+//                9 -> return "LNIVEQWGJNJN7GXL"
+//                10-> return "M1PNUXBG56P9VBOV"
+//                11-> return "LPFYE1HD2ULBRY3T"
+//                12-> return "F7P66V0VOQ5TFH8N"
+//                13-> return "EZSFOHAVM93TJZVJ"
+//                14-> return "HQ1XJSI9C7T9ZQGK"
+//                15-> return "28OX6WFDGCY93HBH"
+//                16-> return "2GM3WMNE96JNFKK6"
+//                17-> return "XTV5KHDB5AL1MAQV"
+//                18-> return "ZLWW2UUXX1M2LSHE"
+
                 0 -> return "AO48IFCXLA3BX1O9"
                 1 -> return "WA7BKM5HXI56RLF6"
                 2 -> return "THYSGY8VEFDN4N57"
@@ -138,6 +160,22 @@ interface AlphaVantageAPI {
                 16-> return "2GM3WMNE96JNFKK6"
                 17-> return "XTV5KHDB5AL1MAQV"
                 18-> return "ZLWW2UUXX1M2LSHE"
+                19-> return "66SHOES29PMPXSH8"
+                20-> return "GPO2P34U9Y7A7HZX"
+                21-> return "4MZVP7YPQKC4UCXM"
+                22-> return "MY7UOXNLMVAJOBL6"
+                23-> return "CUZFO32ID30TEUX6"
+                24-> return "K5EMMM6BVQCN3JU5"
+                25-> return "E55T5S14GV1NN2I3"
+                26-> return "QWWI53Q0A5XH00RC"
+                27-> return "5FG1ZTL7HT3NSBN9"
+                28-> return "I6MIN9Q7N3UD1L8K"
+                29-> return "IMAOA9LVMDPN229X"
+                30-> return "8R2XN1FJNE462AU4"
+                31-> return "C4VZ3V3IA7D3WXVI"
+                32-> return "9UF22PMWEV9BPYJ9"
+                33-> return "Z7B9H271PNLIOWHP"
+                34-> return "YO8GWZOX68E9XL6V"
 
                 else -> return ""
             }
